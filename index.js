0e33129e0dcb19aac58bee365da60a5b559ce9e8
Adrien <adrien@arccoza.com>
2017-10-27 00:40:30 +0900

    Fix anchor indexes off by 1 in `end` fn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function ZipScript(opts, comps) {
 
     if (ctx.length)
       ctx[ctx.length - 1].children.push(cur)
-    else
-      ctx.push(cur)
+    // else
+    //   ctx.push(cur)
   }
 
   function text(strs, ...vals) {
@@ -43,22 +43,20 @@ function ZipScript(opts, comps) {
   }
 
   function end(countOrAnchor=1) {
+    let pre = null
     let idx = anchors[countOrAnchor]
-    idx = idx === undefined ? ctx.length - 1 - countOrAnchor : (anchors[countOrAnchor] = null, idx)
-
-    // assert(idx >= -1 && ctx[idx] !== undefined, `Error changing context, invalid index (${idx}) from: end(${countOrAnchor})`)
-
-    // print('============')
-    // print(ctx[ctx.length - 1] ? ctx[ctx.length - 1].type : ctx[ctx.length - 1])
-
-    // print('-----------')
+    idx = idx === undefined ? ctx.length - countOrAnchor : (anchors[countOrAnchor] = null, idx)
+    
+    assert(typeof idx == 'number', 'Bad index.')
+    --idx
+    
     // Move up through the ctx stack to the selected idx.
     for (let i = ctx.length - 1, v; (v = ctx[i], i > idx); i--) {
       v.children = v.children.map(c => c.type ? h(c.type, c.props, ...c.children) : c)
-      // print(i, v.children.map((c) => c.type ? c.type : c))
     }
+
     cur = ctx[idx]
-    let pre = ctx[idx + 1]
+    pre = ctx[idx + 1]
     ctx = ctx.slice(0, idx + 1)
     
     let {type, props, children} = pre
@@ -75,15 +73,15 @@ function ZipScript(opts, comps) {
   return {...wrapped, z, t: text, text, wrap, start, end}
 }
 
-// var h = require('hyperscript-html').HyperScript()
-var h = require('react').createElement
+var h = require('hyperscript-html').HyperScript()
+// var h = require('react').createElement
 var {z, text, start, end} = ZipScript({h: h})
 var result = null
 
 var time = process.hrtime()
-for(var i = 0; i < 100000; i++) {
+for(var i = 0; i < 1; i++) {
 z('section', null)
-  // start()
+  start()
   z('h1', null, 'Title')
   z('div', null)
     start('content')
@@ -95,7 +93,7 @@ z('section', null)
       // end()
     end('content')
   z('aside')
-  result = end(2) // FIX THIS, shouldn't have to call end with `2`
+  result = end()
 }
 
 print(result)
@@ -103,7 +101,7 @@ print(process.hrtime(time))
 
 
 var time = process.hrtime()
-for(var i = 0; i < 100000; i++) {
+for(var i = 0; i < 1; i++) {
 result = h('section', null,
   h('h1', null, 'Title'),
   h('div', null,
