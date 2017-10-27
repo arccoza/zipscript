000f2b62b04f4a8ed9fe00d7d8a152ee1373245d
Adrien <adrien@arccoza.com>
2017-10-27 16:56:06 +0900

    Update typescript experiment

--- test.ts
+++ test.ts
@@ -6,19 +6,21 @@ interface VNode {
   children: VNode[] | string | number | boolean
 }
 
+type Component = any
+
 type HyperScriptFn =
-  <T, P extends MapOf<any>, C extends VNode>(t: T, p: P, ...c: C[]) => C
+  <T extends Component, P extends MapOf<any>, C extends VNode>(t: T, p: P, ...c: C[]) => C
 
 interface Options {
   h: HyperScriptFn
 }
 
-function ZipScript(opts?: Options, comps?: MapOf<any>) {
+function ZipScript(opts?: Options, comps?: MapOf<Component>) {
   let {h} = opts
   let cur: VNode = null
   let ctx: VNode[] = []
   let anchors: MapOf<number> = {}
-  let wrapped: MapOf<any> = {}
+  let wrapped: MapOf<Component> = {}
 }
 
 let {z, start, end} = ZipScript()
